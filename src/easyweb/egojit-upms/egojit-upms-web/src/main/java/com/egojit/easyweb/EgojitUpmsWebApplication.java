09ff83f0750b19d20b92ffb34f1d8095cf4b7fbc
Asuka <egojit@qq.com>
2017-11-23 16:02:41 +0800

    修改链接

--- src/easyweb/egojit-upms/egojit-upms-web/src/main/java/com/egojit/easyweb/EgojitUpmsWebApplication.java
+++ src/easyweb/egojit-upms/egojit-upms-web/src/main/java/com/egojit/easyweb/EgojitUpmsWebApplication.java
@@ -1,10 +1,15 @@
 package com.egojit.easyweb;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
+
+@EnableEncryptableProperties
 @SpringBootApplication
 @ComponentScan("com.egojit.easyweb")
 @ServletComponentScan
@@ -13,4 +18,5 @@ public class EgojitUpmsWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EgojitUpmsWebApplication.class, args);
 	}
+
 }
--- src/easyweb/egojit-upms/egojit-upms-web/src/test/java/com/egojit/easyweb/EgojitUpmsWebApplicationTests.java
+++ src/easyweb/egojit-upms/egojit-upms-web/src/test/java/com/egojit/easyweb/EgojitUpmsWebApplicationTests.java
@@ -1,7 +1,9 @@
 package com.egojit.easyweb;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +13,14 @@ public class EgojitUpmsWebApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	StringEncryptor stringEncryptor;//密码解码器自动注入
+
+	@Test
+	public void test() {
+		System.out.println(stringEncryptor.encrypt("egojit123"));
 	}
 
 }
